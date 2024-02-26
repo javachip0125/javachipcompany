6a3eeb6ce3653a09e15c404ec4264327d4c0a005
NJH <dldks1212@gmail.com>
2024-02-26 16:43:55 +0900

    commit - ApplicationTest & ManagerDTO

---
+++ src/main/java/run/ApplicationTest.java
@@ -0,0 +1,12 @@
+package run;
+
+import menu.SystemMenu;
+
+public class ApplicationTest {
+  public static void main(String[] args) {
+
+    SystemMenu systemMenu = new SystemMenu();
+
+    systemMenu.loginMenu();
+  }
+}
