c5a25c8478c619a65b8b08e2e80d15cca28969f7
송재원 <ckdlghd3791@naver.com>
2024-02-28 11:14:31 +0900

    commit

--- src/main/java/develop/javachip/menu/SystemMenu.java
+++ src/main/java/develop/javachip/menu/SystemMenu.java
@@ -17,14 +17,10 @@ public class SystemMenu {
   Connection con = getConnection();
 
   private Scanner scanner = new Scanner(System.in);
-  Scanner sc = new Scanner(System.in);
 
   StaffDAO staffDAO = new StaffDAO();
-  ManagerDAO managerDAO = new ManagerDAO();
-
+  ManagerMenu managerMenu = new ManagerMenu();
   StaffMenu staffMenu = new StaffMenu();
-
-  private boolean arriveleave;
 
   public void loginMenu() {
     //scanner = new Scanner(System.in);
@@ -70,12 +66,7 @@ public class SystemMenu {
   //출근하기
 
 
-
-
-
-
   //퇴근하기
-
 
 
   //관리자 메뉴
@@ -90,77 +81,8 @@ public class SystemMenu {
     System.out.println("       오늘의 임직원 근태 현황       ");
     System.out.println("---------------------------------");
 
-    //관리자 메뉴
-    while (true) {
-
-      // 전체 직원 근태정보 조회 내용 출력
-      System.out.println();
-      System.out.println("1. 개별 직원 근태 현황");
-      System.out.println("2. 일정정보");
-      System.out.println("3. 출근정보");
-      System.out.println("4. 당일근무현황");
-      System.out.println("5. 퇴근정보 조회");
-      System.out.println("=================================");
-      System.out.print("메뉴를 선택하세요 : ");
-      int menuNum = 0; // 입력한 메뉴 번호 저장
-      try {
-        menuNum = scanner.nextInt();
-
-        switch (menuNum) {// 올바른 메뉴를 입력한 경우 해당 메뉴 메소드 호출
-          case 1 : // 개별 직원 근태 현황
-            System.out.println("개별 직원 근태 현황 조회 메뉴입니다.");
-            managerDAO.EMPLOYEE_INQUIRY(con);
-            break;
-
-          case 2 : // 일정정보
-            System.out.println("일정정보 메뉴입니다.");
-            break;
-
-          case 3 : // 출근정보
-            System.out.println("출근정보 메뉴입니다.");
-            break;
-
-          case 4 : // 당일근무현황
-            System.out.println("당일근무현황 메뉴입니다.");
-            workStatus();
-
-            break;
-
-          case 5 : // 퇴근정보 조회
-            System.out.println("퇴근정보 메뉴입니다.");
-            break;
-
-          default:// 입력한 메뉴 번호가 올바르지 않은 경우 다시 입력하도록
-            System.out.println("메뉴를 잘못 선택했습니다. 다시 선택해주세요.");
-            break;
-        }
-        break;
-      } catch (InputMismatchException e) { //menuNum에 정수가 아닌 타입으로 입력한 경우
-        System.out.println("메뉴를 잘못 선택했습니다. 다시 선택해주세요.");
-        scanner.nextLine(); // Scanner에 입력되어있던 내용 버퍼 비우기
-      }
-    }
+    managerMenu.managerMenu();
   }
-
-
-  // 개별 직원 근태 현황
-
-
-  // 일정정보
-
-
-  // 출근정보
-
-
-  // 당일근무현황 : 전체직원에 대한 조회 만들기
-  public void workStatus(){
-    System.out.println("전체직원에 대한 당일근무현황 조회한 결과입니다.");
-    managerDAO.selectWorkStatus(con);
-  }
-
-
-
-  // 퇴근정보 조회
 
 
   //직원 메뉴
