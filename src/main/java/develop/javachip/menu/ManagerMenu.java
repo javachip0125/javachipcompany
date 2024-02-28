64b4de8193e48ffadf1c211b0fb6d9f05a0ab049
송재원 <ckdlghd3791@naver.com>
2024-02-28 17:36:14 +0900

    MANAGER MENU - showSchedule

--- src/main/java/develop/javachip/menu/ManagerMenu.java
+++ src/main/java/develop/javachip/menu/ManagerMenu.java
@@ -43,6 +43,7 @@ public class ManagerMenu {
 
                     case 2: // 일정정보
                         System.out.println("일정정보 메뉴입니다.");
+                        showSchedule();
                         break;
 
                     case 3: // 출근정보
@@ -83,6 +84,84 @@ public class ManagerMenu {
                 }
             } catch (InputMismatchException e) { //menuNum에 정수가 아닌 타입으로 입력한 경우
                 System.out.println("메뉴를 잘못 선택했습니다. 다시 선택해주세요.");
+                scanner.nextLine(); // Scanner에 입력되어있던 내용 버퍼 비우기
+            }
+        }
+    }
+    public void showSchedule() {
+        System.out.println("=================================");
+        System.out.println("              일정조회            ");
+        System.out.println("=================================");
+        System.out.println("1. 오늘 일정\n2. 다음주 일정");
+        System.out.println("=================================");
+
+        while (true) {
+            System.out.print("조회할 일정을 선택해주세요 : ");
+
+            int menuNum = 0;
+            String select = "";
+            char answer;
+            char answer2;
+
+            try {
+                menuNum = scanner.nextInt();
+
+                switch (menuNum) {
+                    case 1:
+                        select = "금요일";
+                        managerDAO.selectAllTodaySchedule(con, select);
+
+                        while (true) {
+                            System.out.print("이전메뉴로 돌아갈까요?(N/Y) : ");
+
+                            try {
+                                answer = scanner.next().toUpperCase().charAt(0);
+                                if (answer == 'Y') {
+                                    System.out.println("이전메뉴로 돌아갑니다.");
+                                    break;
+                                }
+                            } catch (InputMismatchException e) { //char 타입 아닌 리터럴 입력시
+                                System.out.println("잘못 선택했습니다.\n다시 선택해주세요.");
+                                System.out.println("---------------------------------");
+                                scanner.nextLine(); // Scanner에 입력되어있던 내용 버퍼 비우기
+                            }
+                        }
+                        break;
+                    case 2:
+                        //다음주 일정 조회 메소드 호출
+                        managerDAO.selectAllNextSchedule(con);
+
+                        while (true) {
+                            System.out.print("일정을 다시 조회하시겠습니까?(N/Y) : ");
+
+                            try {
+                                answer2 = scanner.next().toUpperCase().charAt(0);
+                                if (answer2 == 'N') {
+                                    System.out.println("이전메뉴로 돌아갑니다.");
+                                    break;
+                                } else {
+                                    System.out.println("일정조회를 다시 진행합니다.");
+                                    managerDAO.selectAllNextSchedule(con); // 다음주 일정 조회 메소드 호출
+                                }
+                            } catch (InputMismatchException e) { //char 타입 아닌 리터럴 입력시
+                                System.out.println("잘못 선택했습니다.\n다시 선택해주세요.");
+                                System.out.println("---------------------------------");
+                                scanner.nextLine(); // Scanner에 입력되어있던 내용 버퍼 비우기
+                            }
+                        }
+                        break;
+                }
+
+                if (menuNum > 2 || menuNum < 0) {
+                    System.out.println("잘못 선택했습니다.\n다시 선택해주세요.");
+                    System.out.println("---------------------------------");
+                } else {
+                    break;
+                }
+
+            } catch (InputMismatchException e) { //menuNum에 정수가 아닌 타입으로 입력한 경우
+                System.out.println("잘못 선택했습니다.\n다시 선택해주세요.");
+                System.out.println("---------------------------------");
                 scanner.nextLine(); // Scanner에 입력되어있던 내용 버퍼 비우기
             }
         }
